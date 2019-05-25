e156d50300c9d8f434536a82ec7b53e618473717
vodelerk <lallamavioleta@gmail.com>
2019-05-24 19:47:13 -0500

    Added EF and employee model

--- MVCRegister/MVCRegister/Startup.cs
+++ MVCRegister/MVCRegister/Startup.cs
@@ -7,8 +7,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using MVCRegister.Models;
 
 namespace MVCRegister
 {
@@ -33,6 +35,7 @@ namespace MVCRegister
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddDbContext<EmployeeContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DevConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
